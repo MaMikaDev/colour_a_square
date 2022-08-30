97b9fe86f3777bea5eff3bfbdf6dbe3f83e46186
MK <67931479+TheOtherMK@users.noreply.github.com>
2022-08-30 21:37:53 +0100

    a semi-working grid

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-const container = document.querySelector('#grid');
-
-container.setAttribute('style', 'display: grid; grid-template-columns: auto auto;')
+const grid = document.querySelector('#grid');
 
 
-    for (let i = 0; i < 17; i++) {
-        let aColumn = document.createElement('div');
+// grid.setAttribute('style', 'display: grid; grid-template-columns: auto auto;')
 
-        aColumn.id = 'column' + i;
-        console.log(aColumn.id)
-        aColumn.setAttribute('style', 'border: solid black; background: white;');  
+
+    for (let i = 0; i < 16; i++) {  //repat the process exactly 16 times
+
+        let aRow = document.createElement('div'); //create a row
+
+        aRow.classList.add('row');
+        aRow.id = 'row' + i; //each row gets a consequtive number
+        console.log(aRow.id)  // checking if the rows are being intiated correctly
+        aRow.setAttribute('style', 'border: solid black; background: white;');  
        
+        aRow.textContent = 'I need to figure it out lol ' + i;
 
-        container.appendChild(aColumn);
+        const rows = document.getElementsByClassName('row');
 
-        let aRow = document.getElementById('column' + i);
+            for(let j = 0; j < 16; j++){
+                let aCell = document.createElement('div'); //create a cell div
+                aCell.id = 'r' + i + 'c' + j; //naming each cell for checking purposes really
 
-        for(let j = 0; j < 17; j++){
+                console.log(aCell.id);
 
-            let aCell = document.createElement('div');
-            aCell.textContent = 'Hi';
+                aCell.setAttribute('style', 'border: solid pink; background: yellow;');
+                aCell.textContent = 'hello?';
 
-            aRow.appendChild(aCell);
-    
+                aRow.appendChild(aCell);
+            }
+
+            grid.appendChild(aRow);  //attach the row to the grid
+
         }
-
-        
-
-    }
