e47d7d08245de6107e1eb16970adfad32cc72a10
theOtherMk <67931479+TheOtherMK@users.noreply.github.com>
2022-08-30 10:29:18 +0100

    typos removed

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const container = document.querySelector('#container');
 
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This was added in JavaScript!';
+
+container.appendChild(content);
