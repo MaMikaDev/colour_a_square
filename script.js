a89aac7a562479cd25e07abafb7d4380eace9195
MK <67931479+TheOtherMK@users.noreply.github.com>
2022-08-30 19:05:26 +0100

    attempt to create a draft of a grid using loops

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('#grid');
 
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'This was added in JavaScript!';
+container.setAttribute('style', 'display: grid; grid-template-columns: auto auto;')
 
-container.appendChild(content);
+
+    for (let i = 0; i < 17; i++) {
+        let aColumn = document.createElement('div');
+
+        aColumn.id = 'column' + i;
+        console.log(aColumn.id)
+        aColumn.setAttribute('style', 'border: solid black; background: white;');  
+       
+
+        container.appendChild(aColumn);
+
+        let aRow = document.getElementById('column' + i);
+
+        for(let j = 0; j < 17; j++){
+
+            let aCell = document.createElement('div');
+            aCell.textContent = 'Hi';
+
+            aRow.appendChild(aCell);
+    
+        }
+
+        
+
+    }
